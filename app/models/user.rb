a8db6368719efa00214d917f8b1df96696a69d66
potatolight <shuguangbai@gmail.com>
2020-09-22 20:51:58 +0000

    add uniqueness for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
-
+  validates :email, :uniqueness => { :case_sensitive => false }
+  validates :email, presence: true
   validates :email, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
